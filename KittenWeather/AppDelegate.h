a83ac479b2614a431c13e7605d35c2e8cd947765
Sean McMains <sean@mcmains.net>
2015-02-12 08:54:52 -0600

    Initial Commit

---
+++ KittenWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KittenWeather
+//
+//  Created by Sean McMains on 2/12/15.
+//  Copyright (c) 2015 Mutual Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
